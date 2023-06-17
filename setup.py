454fa4d24c2c3bbb48419beeb518089ab6179dab
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-06-17 16:19:24 -0700

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="llmfuncs",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/FyZyX/llmfuncs",
     author="Lucas Lofaro",
     author_email="lucasmlofaro@gmail.com",
