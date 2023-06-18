42527f132ceb1398b2e59a62b4c3de28a3e6278d
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-06-17 17:47:13 -0700

    add JSON parsing and error handling

--- src/llmfuncs/validator.py
+++ src/llmfuncs/validator.py
@@ -1,6 +1,19 @@
+import json
+
 import jsonschema
 
 
-def call_function_with_validation(schema, func, args):
-    jsonschema.validate(args, schema)
+def parse_json(json_string):
+    try:
+        return json.loads(json_string)
+    except json.JSONDecodeError as e:
+        raise ValueError(f"Failed to parse JSON: {e}")
+
+
+def call_function_with_validation(schema, func, json_string):
+    args = parse_json(json_string)
+    try:
+        jsonschema.validate(args, schema)
+    except jsonschema.ValidationError as e:
+        raise ValueError(f"Failed to validate JSON: {e}")
     return func(**args)
