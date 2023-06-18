7a6dc6a8d04bad052e4f520720c729e6a3ef81e6
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-06-17 21:33:05 -0700

    use tool collection in OpenAI example

--- examples/openai_example.py
+++ examples/openai_example.py
@@ -2,7 +2,7 @@ import json
 
 import openai
 
-from llmfuncs import schema, validator
+from llmfuncs.tool import Tool, ToolCollection
 
 
 # Define your function
@@ -19,14 +19,11 @@ def get_current_weather(location, unit="fahrenheit"):
     return json.dumps(weather_info)
 
 
-# Generate the schema for your function
-function_schema = schema.from_function(get_current_weather)
-
-
 def run_conversation(model="gpt-3.5-turbo-0613"):
     # Step 1: send the conversation and available functions to GPT
     messages = [{"role": "user", "content": "What's the weather like in Boston?"}]
-    functions = [function_schema]  # Use the schema generated from your function
+    tools = ToolCollection(tools=[Tool(get_current_weather)])
+    functions = tools.schema()  # Use the schema generated from your function
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
@@ -40,29 +37,22 @@ def run_conversation(model="gpt-3.5-turbo-0613"):
         # Step 3: call the function
         function_name = response_message["function_call"]["name"]
         function_args_json = response_message["function_call"]["arguments"]
+        function_response = tools.use_tool(function_name, function_args_json)
 
-        if function_name == "get_current_weather":
-            # validate the arguments against the schema and call the function
-            function_response = validator.validate_and_call(
-                func=get_current_weather,
-                args_json=function_args_json,
-                schema=function_schema["parameters"],
-            )
-
-            # Step 4: send the info on the function call and function response to GPT
-            messages.append(response_message)  # extend conversation with assistant's reply
-            messages.append(
-                {
-                    "role": "function",
-                    "name": function_name,
-                    "content": function_response,
-                }
-            )  # extend conversation with function response
-            second_response = openai.ChatCompletion.create(
-                model=model,
-                messages=messages,
-            )  # get a new response from GPT where it can see the function response
-            return second_response
+        # Step 4: send the info on the function call and function response to GPT
+        messages.append(response_message)  # extend conversation with assistant's reply
+        messages.append(
+            {
+                "role": "function",
+                "name": function_name,
+                "content": function_response,
+            }
+        )  # extend conversation with function response
+        second_response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages,
+        )  # get a new response from GPT where it can see the function response
+        return second_response
 
 
 print(run_conversation())
